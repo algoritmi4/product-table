01ca7f5aeae18671dce2d6dffe4f52a93a1c60f8
Ivan <ButorinLP@yandex.ru>
2024-02-25 17:50:45 +0300

    feat: vite init and add axios baseApi

---
+++ src/utils/api/lib/md5ConvertFunc.ts
@@ -0,0 +1,16 @@
+import md5 from "md5";
+
+// env const in future
+const password = "Valantis";
+
+export const getmd5Header = () => {
+  const date = new Date();
+  const currentMonth = date.getUTCMonth() + 1;
+  const currentDay = date.getUTCDate();
+  const currentMonthLength = String(currentMonth).length;
+  const currentDayLength = String(currentDay).length;
+
+  const md5String = `${password}_${date.getUTCFullYear()}${currentMonthLength < 2 ? "0" : ""}${currentMonth}${currentDayLength < 2 ? "0" : ""}${currentDay}`;
+
+  return md5(md5String);
+};
